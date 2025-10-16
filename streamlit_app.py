47b088ee933f0514a397f71635cf12dde500485c
DLAIE Burner <dlaieburner@gmail.com>
2025-10-15 23:23:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,10 +89,12 @@ def display_leaderboard(use_streamlit=True):
     
     st.title("🏆 2025 DLAIE Latent Flow Matching Leaderboard - Open Version")
 
-    st.markdown('''
+    st.markdown('''This is the "open public version" of an in-class contest for Deep Learning & AI Ethics at Belmont University<br>
         <div style="display: flex; flex-wrap: wrap; justify-content: space-between; align-items: center; gap: 20px;">
             <div style="flex: 1; min-width: 250px;">
-                <h4><a href="https://github.com/DLAIE/2025-LeaderboardContest">Contest Page</a> | 
+                <h4>
+                <a href="https://2025-dlaie-leaderboard.streamlit.app/">Original Leaderboard</a> |
+                <a href="https://github.com/DLAIE/2025-LeaderboardContest">Original Contest Page</a> | 
                 <a href="https://github.com/drscotthawley/DLAIE">DLAIE Course Page</a></h4>
             </div>
             <div style="flex: 0 0 auto;">
